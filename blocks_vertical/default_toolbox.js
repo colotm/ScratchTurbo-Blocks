6dc22c178fe6e20249522500496f0dbc1982d362
Titima <colonruizanabel@gmail.com>
2026-06-11 15:12:44 -0400

    Add control_switch and update control_run_as_sprite block

--- blocks_vertical/default_toolbox.js
+++ blocks_vertical/default_toolbox.js
@@ -304,10 +304,11 @@ Blockly.Blocks.defaultToolbox = '<xml id="toolbox-categories" style="display: no
     '<block type="control_forever" id="control_forever"></block>' +
     '<block type="control_if" id="control_if"></block>' +
     '<block type="control_if_else" id="control_if_else"></block>' +
+    '<block type="control_switch" id="control_switch"></block>' +
     '<block type="control_wait_until" id="control_wait_until"></block>' +
     '<block type="control_repeat_until" id="control_repeat_until"></block>' +
     '<block type="control_stop" id="control_stop"></block>' +
-      '<block type="control_repeat_until" id="control_run_as_sprite"></block>' +
+      '<block type="control_run_as_sprite" id="control_run_as_sprite"></block>' +
     '<block type="control_start_as_clone" id="control_start_as_clone"></block>' +
     '<block type="control_create_clone_of" id="control_create_clone_of">' +
       '<value name="CLONE_OPTION">' +
